a86b9b5b23d8013b1fd31d51680825f6e7200ac2
wangchao <magician.of.technique@gmail.com>
2022-12-15 18:37:13 +0800

    [*] fix 配置

--- redis/docker-run-4.0.10.sh
+++ redis/docker-run-4.0.10.sh
@@ -6,13 +6,13 @@ currentPath=`cd $(dirname $0);pwd -P`
 
 mkdir -p /ingot-data/docker/volumes/redis-4.0.10/conf
 mkdir -p /ingot-data/docker/volumes/redis-4.0.10/data
-cp ${currentPath}/ingot-redis.conf /ingot-data/docker/volumes/redis-4.0.10/conf
+cp ${currentPath}/redis-4.0.10.conf /ingot-data/docker/volumes/redis-4.0.10/conf
 
 
 docker run --name redis \
  --restart always \
  --network ingot-net --ip 172.88.0.90 \
  -p 6379:6379 \
- -v /ingot-data/docker/volumes/redis-4.0.10/conf/ingot-redis.conf:/etc/redis/redis.conf \
+ -v /ingot-data/docker/volumes/redis-4.0.10/conf/redis-4.0.10.conf:/etc/redis/redis.conf \
  -v /ingot-data/docker/volumes/redis-4.0.10/data:/data \
  -d redis:4.0.10 redis-server /etc/redis/redis.conf --appendonly yes
