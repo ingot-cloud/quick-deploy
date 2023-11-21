b000a4f11b2b3c0b01e2d1cfd9f28d9cfb074f8a
jy <magician.of.technique@aliyun.com>
2023-11-21 09:13:10 +0800

    [*] fix minio

--- minio/docker-run-domain.sh
+++ minio/docker-run-domain.sh
@@ -3,11 +3,10 @@
 
 currentPath=`cd $(dirname $0);pwd -P`
 
-
 VIRTUAL_HOST=minio.ingotcloud.top
 VIRTUAL_PORT=5001
 
-minio_version=2023.11.15
+minio_version=latest
 default_user=admin
 default_user_pwd=12345678
 
@@ -16,11 +15,10 @@ mkdir -p /ingot-data/docker/volumes/minio/config
 
 docker run --name minio \
      --network ingot-net --ip 172.88.0.150 \
-     -p 5000:9000 \
      -d --restart=always \
      -e VIRTUAL_HOST=${VIRTUAL_HOST} \
      -e VIRTUAL_PORT=${VIRTUAL_PORT} \
      -e MINIO_ROOT_USER=${default_user} \
      -e MINIO_ROOT_PASSWORD=${default_user_pwd} \
-     -v /ingot-data/docker/volumes/minio/data:/bitnami/minio/data \
-     bitnami/minio:${minio_version}
+     -v /ingot-data/docker/volumes/minio/data:/data \
+     minio/minio:${minio_version} 'server' '/data' '--console-address' ':5001' '-address' ':9000'
--- minio/docker-run.sh
+++ minio/docker-run.sh
@@ -3,7 +3,7 @@
 
 currentPath=`cd $(dirname $0);pwd -P`
 
-minio_version=`2023.11.15`
+minio_version=latest
 default_user=admin
 default_user_pwd=12345678
 
@@ -12,10 +12,10 @@ mkdir -p /ingot-data/docker/volumes/minio/config
 
 docker run --name minio \
      --network ingot-net --ip 172.88.0.150 \
+     -p 6001:5001 \
      -p 9090:9000 \
-     -p 6001:9001 \
      -d --restart=always \
      -e MINIO_ROOT_USER=${default_user} \
      -e MINIO_ROOT_PASSWORD=${default_user_pwd} \
-     -v /ingot-data/docker/volumes/minio/data:/bitnami/minio/data \
-     bitnami/minio:${minio_version}
+     -v /ingot-data/docker/volumes/minio/data:/data \
+     minio/minio:${minio_version} 'server' '/data' '--console-address' ':5001' '-address' ':9000'
