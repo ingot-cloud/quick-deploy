c12f1ff09592abc9230d0beef61f4ed020248851
jy <magician.of.technique@aliyun.com>
2024-03-08 08:51:03 +0800

    [*] update registry

--- registry/docker-run.sh
+++ registry/docker-run.sh
@@ -6,10 +6,12 @@ currentPath=`cd $(dirname $0);pwd -P`
 
 mkdir -p /ingot-data/docker/volumes/registry/images
 mkdir -p /ingot-data/docker/volumes/registry/auth
+mkdir -p /ingot-data/docker/volumes/registry/config
 
 
 # 配置htpasswd
 cp ${currentPath}/auth/* /ingot-data/docker/volumes/registry/auth
+cp ${currentPath}/default.yml /ingot-data/docker/volumes/registry/config
 
 
 VIRTUAL_HOST=docker-registry.ingotcloud.top
@@ -20,6 +22,7 @@ docker run --name registry \
  --network ingot-net --ip 172.88.0.5 \
  --restart=always \
  --privileged=true \
+ -v /ingot-data/docker/volumes/registry/config/default.yml:/etc/docker/registry/config.yml \
  -v /ingot-data/docker/volumes/registry/images:/var/lib/registry \
  -v /ingot-data/docker/volumes/registry/auth:/auth \
  -e VIRTUAL_HOST=${VIRTUAL_HOST} \
