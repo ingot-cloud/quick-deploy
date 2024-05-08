9c2f3e2b4cb8362d44bec734785d3164db55de54
jy <magician.of.technique@aliyun.com>
2024-05-08 11:01:06 +0800

    [*] 提交脚本

---
+++ es/docker-run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run --name elasticsearch \
+ -d --restart=always \
+ -p 9200:9200 \
+ -p 9300:9300 \
+ -e "discovery.type=single-node" \
+ docker.elastic.co/elasticsearch/elasticsearch:8.1.0
