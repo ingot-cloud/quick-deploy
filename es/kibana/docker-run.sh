0ce490ca4d850875fb5640886bff6ff7f3bf15df
jy <magician.of.technique@aliyun.com>
2024-05-09 09:20:50 +0800

    [*] fix config

--- es/kibana/docker-run.sh
+++ es/kibana/docker-run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker run --name kibana \
+ -d --restart=always \
  -p 5601:5601 \
  -e "ELASTICSEARCH_HOSTS=http://es01-test:9200" \
  docker.elastic.co/kibana/kibana:7.17.21
