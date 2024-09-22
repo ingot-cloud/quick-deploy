e915c47aa74591663865a432a037c7a42b1aa14c
jy <magician.of.technique@aliyun.com>
2024-09-22 10:20:26 +0800

    New: api代理域名配置

---
+++ api-proxy/build-api.sh
@@ -0,0 +1,9 @@
+
+INNER_VERSION=0.1.0
+MODULE_NAME=wande62-api
+
+# source ./.local_env
+
+docker login -u ${JY_DOCKER_REGISTRY_USERNAME} -p ${JY_DOCKER_REGISTRY_PASSWORD} docker-registry.ingotcloud.top
+docker buildx build --platform linux/amd64 -t docker-registry.ingotcloud.top/ingot/${MODULE_NAME}:${INNER_VERSION} .
+docker push docker-registry.ingotcloud.top/ingot/${MODULE_NAME}:${INNER_VERSION}
