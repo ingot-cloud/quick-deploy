a8745d291fecc67708fb4e350850d29927da02f7
jy <magician.of.technique@aliyun.com>
2025-04-14 09:50:57 +0800

    Fix: mysql image 8.0.41

--- mysql/docker-run.sh
+++ mysql/docker-run.sh
@@ -2,15 +2,16 @@
 
 
 currentPath=`cd $(dirname $0);pwd -P`
+MYSQL_IMAGE=mysql:8.0.41 # mysql/mysql-server:8.0.32
+MYSQL_ROOT_PASSWORD=123456
+MYSQL_VALUME_PATH=/ingot-data/docker/volumes/mysql/volumes
 
-
-mkdir -p /ingot-data/docker/volumes/mysql-8.0.32/volumes
-
+mkdir -p ${MYSQL_VALUME_PATH}
 
 docker run --name mysql-8.0.32 \
  --network ingot-net --ip 172.88.0.10 \
- --restart always \
+ -d --restart always \
  -p 3306:3306 \
- -e MYSQL_ROOT_PASSWORD=123456 \
- -v /ingot-data/docker/volumes/mysql-8.0.32/volumes:/var/lib/mysql \
- -d mysql/mysql-server:8.0.32
+ -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+ -v ${MYSQL_VALUME_PATH}:/var/lib/mysql \
+ ${MYSQL_IMAGE}
