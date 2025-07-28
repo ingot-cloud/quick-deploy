1133fb99988241447db8c8118f854dc8bcabda39
jy <magician.of.technique@aliyun.com>
2025-07-28 08:29:37 +0800

    Fix: 修改portainer sh

--- portainer/docker-run.sh
+++ portainer/docker-run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+VERSION=2.27.9
+IMAGE_NAME=portainer/portainer-ce
+SERVICE_NAME=portainer
+
+# 停止运行当前容器
+docker ps -q --filter name="${SERVICE_NAME}" | xargs -r docker rm -f
+# 删除当前镜像
+docker images -q --filter reference="${imageName}" | xargs -r docker rmi -f
 
 docker run -d --name portainer \
  --network ingot-net \
@@ -8,4 +16,4 @@ docker run -d --name portainer \
  -e AGENT_SECRET=123456 \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v portainer_data:/data \
- portainer/portainer-ce
+ portainer/portainer-ce:${VERSION}
