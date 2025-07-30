6e070880636c554fe8b4d56d7306980544970c4d
jy <magician.of.technique@aliyun.com>
2025-07-30 10:16:04 +0800

    Fix: 单节点配置优化

--- kafka/single/run.sh
+++ kafka/single/run.sh
@@ -2,6 +2,8 @@
 
 currentPath=`cd $(dirname $0);pwd -P`
 
-mkdir -p /ingot-data/docker/volumes/kafka/broker
+source ./.env
+
+mkdir -p /ingot-data/docker/volumes/kafka/${BROKER1_NAME}
 
 docker compose up -d
