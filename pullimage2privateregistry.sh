df4eccfd0e4dd47639c45c1af53d35ad9f7a6134
jy <magician.of.technique@aliyun.com>
2025-10-16 17:00:52 +0800

    Fix: image

--- pullimage2privateregistry.sh
+++ pullimage2privateregistry.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 拉取镜像并且推送到私有仓库,保持多架构
-IMAGE=nginxproxy/acme-companion
-TAG=2.6
+IMAGE=wurstmeister/kafka
+TAG=2.13-2.8.1
 PRIVATE_REGISTRY=docker-registry.ingotcloud.top
 
 # 1. 分别拉取两种架构的镜像并标记
