2606591aa3f2d0af4ba42363fd080eb1e30cba29
jy <magician.of.technique@aliyun.com>
2025-12-05 16:41:17 +0800

    docs: 增加日志

--- kafka/cluster/run.sh
+++ kafka/cluster/run.sh
@@ -4,8 +4,17 @@ currentPath=`cd $(dirname $0);pwd -P`
 
 source ./.env
 
+# 给 Kafka 用户赋权限, 注意UID和GID，这里用的是1000
 mkdir -p ${CLUSTER_BROKER1_VOLUME}
+chown -R 1000:1000 ${CLUSTER_BROKER1_VOLUME}
+chmod -R 755 ${CLUSTER_BROKER1_VOLUME}
+
 mkdir -p ${CLUSTER_BROKER2_VOLUME}
+chown -R 1000:1000 ${CLUSTER_BROKER2_VOLUME}
+chmod -R 755 ${CLUSTER_BROKER2_VOLUME}
+
 mkdir -p ${CLUSTER_BROKER3_VOLUME}
+chown -R 1000:1000 ${CLUSTER_BROKER3_VOLUME}
+chmod -R 755 ${CLUSTER_BROKER3_VOLUME}
 
 docker compose up -d
--- kafka/single/run.sh
+++ kafka/single/run.sh
@@ -5,5 +5,7 @@ currentPath=`cd $(dirname $0);pwd -P`
 source ./.env
 
 mkdir -p ${BROKER1_VOLUME}
+chown -R 1000:1000 ${BROKER1_VOLUME}
+chmod -R 755 ${BROKER1_VOLUME}
 
 docker compose up -d
