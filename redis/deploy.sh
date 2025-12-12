9a1412c04357c9eefc2e91945a7f0801c1c2447b
jy <magician.of.technique@aliyun.com>
2025-12-12 16:46:17 +0800

    chore: 修改默认值

--- redis/deploy.sh
+++ redis/deploy.sh
@@ -25,7 +25,7 @@ docker rm ${CONTEINER_NAME:-redis} 2>/dev/null || true
 # Run Redis container
 CONTAINER_ID=$(docker run -d \
   --name ${CONTEINER_NAME:-redis} \
-  --network ${DOCKER_NETWORK:-dockerun-net} \
+  --network ${DOCKER_NETWORK:-ingot-net} \
   --ip ${DOCKER_NETWORK_IP:-172.88.0.90} \
   -p ${REDIS_PORT:-6379}:6379 \
   -v ${WORK_DIR}/volumes/data:/data \
